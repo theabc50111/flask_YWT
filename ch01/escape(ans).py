c089c35f632d237e29ae2b980e9ff95cc12babd7
YWT <theabc50111@gmail.com>
2021-03-06 07:26:46 +0800

    ch01/dynamic_route_type

--- ch01/escape(ans).py
+++ ch01/escape(ans).py
@@ -9,10 +9,14 @@ def index():
     return '<h1>Hello World!</h1>'
 
 # ----------practice start------------
-@app.route('/user/<name>/<int:id>')
-def user_check_type(name, id):
-    return (f'<h1>id: {escape(type(id))}!</h1>'
-            f'<h1>name: {escape(type(name))}!</h1>')
+@app.route('/escape')
+def with_escape():
+    return escape("<h1>With escape()</h1>")
+            
+
+@app.route('/not_escape')
+def not_with_escape():
+    return "&lt;h1&gt;Not with escape()&lt;/h1&gt;"
 # ----------practice end--------------
 
 if __name__ == '__main__':
