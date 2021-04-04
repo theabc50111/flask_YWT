33e8e7cd70e37e43470ef442c6997e2c01727755
YWT <theabc50111@gmail.com>
2021-04-05 01:04:02 +0800

    revise ch07 error_handler.py

--- ch07/error_handler(practice).py
+++ ch07/error_handler(practice).py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template
-from flask_moment import Moment
-from datetime import datetime
 
 app = Flask(__name__)
-moment = Moment(app)
 
 
 @app.route('/')
 def index():
     return render_template('moment.html',
-                           page_header="page_header",
-                           current_time=datetime.utcnow())
+                           page_header="page_header")
 
 
 # practice start
