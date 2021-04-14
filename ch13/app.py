954eec623b59f67079be5ad7b48d88b99741628f
YWT <theabc50111@gmail.com>
2021-04-15 07:57:05 +0800

    new ch13

---
+++ ch13/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, url_for
+from flask_moment import Moment
+from datetime import datetime
+import os
+import uuid
+
+# practice start
+import recognition.load_model as model
+# practice end
+
+app = Flask(__name__)
+moment = Moment(app)
+
+# practice start
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))+r'/static/uploaded'
+# practice end
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           page_header="page_header",
+                           current_time=datetime.utcnow())
+
+# practice start
+@app.route('/rec', methods=['GET', 'POST'])
+def get_file():
+    if request.method == "GET":
+        return render_template('file.html', page_header="upload hand write picture")
+    elif request.method == "POST":
+        file = request.files['file']
+        if file:
+            filename = str(uuid.uuid4())+"_"+file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            predict = model.recog_digit(filename)
+        return render_template('recog_result.html', page_header="hand writing digit recognition", predict = predict, src = url_for('static', filename=f'uploaded/{filename}'))
+# practice end
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
---
+++ ch13/recognition/load_model.py
@@ -0,0 +1,16 @@
+import pickle
+from PIL import Image
+import PIL.ImageOps 
+import numpy as np
+import os
+
+directory = os.path.dirname(os.path.abspath(__file__))
+
+def recog_digit(pic_name):
+    with open(directory+"/"+"model.pickle", 'rb') as f:
+        clf_load = pickle.load(f)
+    image = Image.open(directory+"/../static/uploaded/"+pic_name).resize((28,28))
+    inverted_image = PIL.ImageOps.invert(image.convert('RGB'))
+    np_img = np.array(inverted_image)[:,:,0]
+    data = np_img.reshape((1,-1))
+    return clf_load.predict(data)
