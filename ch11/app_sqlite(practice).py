6027bdb81b9b1005194b294c5301aef1b4280ccd
YWT <theabc50111@gmail.com>
2021-04-27 03:46:56 +0800

    revise ch11

---
+++ ch11/app_sqlite(practice).py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_moment import Moment
+from datetime import datetime
+import sqlalchemy as db
+from sqlalchemy import func
+import math
+
+
+app = Flask(__name__)
+moment = Moment(app)
+
+# sql setting
+path_to_db = "./db/chinook.db"
+table = 'customers'
+engine = db.create_engine(f'sqlite:///{path_to_db}')
+metadata = db.MetaData()
+table_customers = db.Table(table, metadata, autoload=True, autoload_with=engine)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           page_header="page_header",
+                           current_time=datetime.utcnow())
+
+
+#----------practice start------------
+
+#----------practice end--------------
+
+if __name__ == "__main__":
+    app.run(debug=True)
