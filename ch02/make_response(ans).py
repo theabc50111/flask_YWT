ca52eabcfb79be55149f8fb4149e365b0ff3a88e
YWT <theabc50111@gmail.com>
2021-09-20 19:34:10 +0800

    revise ch02, ch04

--- ch02/make_response(ans).py
+++ ch02/make_response(ans).py
@@ -11,7 +11,7 @@ def index():
     response = make_response('<h1>Check the status code</h1>')
     response.status_code = 200
     print("response.content_length :",response.content_length, 
-          "len(\"<h1>Check the status code</h1>\")",len("<h1>Check the status code</h1>"))
+          "len(\"<h1>Check the status code</h1>\"):",len("<h1>Check the status code</h1>"))
     return response
 #----------practice end--------------
 
