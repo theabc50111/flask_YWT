faeb9d3b8deca513fdea89b5f2e87d2a9db421b4
YWT <theabc50111@gmail.com>
2023-04-14 12:10:24 +0800

    remove flask-moment for ch07

--- ch07/file(practice).py
+++ ch07/file(practice).py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request
-from flask_moment import Moment
 from datetime import datetime
 
 # practice start
 # practice end
 
 app = Flask(__name__)
-moment = Moment(app)
 
 # practice start
 # practice end
@@ -14,8 +12,7 @@ moment = Moment(app)
 @app.route('/')
 def index():
     return render_template('index.html',
-                           page_header="page_header",
-                           current_time=datetime.utcnow())
+                           page_header="page_header")
 
 
 # practice start
--- ch07/wtf_form(practice).py
+++ ch07/wtf_form(practice).py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask_moment import Moment
 from datetime import datetime
 
 # practice start
@@ -7,7 +6,6 @@ from datetime import datetime
 # practice end
 
 app = Flask(__name__)
-moment = Moment(app)
 
 # practice start
 
@@ -16,8 +14,7 @@ moment = Moment(app)
 @app.route('/')
 def index():
     return render_template('index.html',
-                           page_header="page_header",
-                           current_time=datetime.utcnow())
+                           page_header="page_header")
 
 # practice start
 
