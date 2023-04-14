5a175332dc66f5b484b822fae92264c0473de594
YWT <theabc50111@gmail.com>
2023-04-14 12:19:17 +0800

    remove flask-moment for ch08

--- ch08/app.py
+++ ch08/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, url_for
-from flask_moment import Moment
 from datetime import datetime
 from pathlib import Path
 import uuid
@@ -9,7 +8,6 @@ import recognition.load_model as model
 # practice end
 
 app = Flask(__name__)
-moment = Moment(app)
 
 # practice start
 UPLOAD_FOLDER = Path(__file__).resolve().parent/'static/uploaded'
@@ -21,8 +19,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
 @app.route('/')
 def index():
     return render_template('index.html',
-                           page_header="page_header",
-                           current_time=datetime.utcnow())
+                           page_header="page_header")
 
 # practice start
 @app.route('/rec', methods=['GET', 'POST'])
